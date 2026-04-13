4968ac42b4b90270245909dd0ceed9e5aec661b9
ayubkhona0-alt <ayubkhona0@gmail.com>
2026-04-14 03:36:04 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL_ID = "@beshsomm"
+
+def get_cny_rate():
+    url = "https://open.er-api.com/v6/latest/CNY"
+    data = requests.get(url).json()
+    return data["rates"]["UZS"]
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHANNEL_ID,
+        "text": text
+    }
+    requests.post(url, data=payload)
+
+def main():
+    rate = get_cny_rate()
+    text = f"💴 Курс юаня\n\n1 CNY = {rate} UZS"
+    send_message(text)
+
+if name == "__main__":
+    main()
